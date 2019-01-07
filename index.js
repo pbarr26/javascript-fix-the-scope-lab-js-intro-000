d98f9c97371893fb0e207e54c0cd1bf695d4de14
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-01-07 02:50:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = innerVariable("FUNKY!") 
+var theFunk = innerVariable("FUNKY!")
 innerVariable()
-
