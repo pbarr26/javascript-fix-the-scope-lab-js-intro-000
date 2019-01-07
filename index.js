0201bda6e795c3b3b59ad02c85ce3bd63616aca8
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-01-07 02:18:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ function myAnimal() {
   return animal
 }
 
-function yourAnimal() {
+function yourAnimal(cat) {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  var innerVariable= 'cat';
+  return innerVariable;
 }
 
-function add2(n) {
+
+function add2(n) const two = 2 {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+
 }
 
 var funkyFunction = function() {
