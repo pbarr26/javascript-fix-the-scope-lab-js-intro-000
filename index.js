c7b4fa5e5ae153b6d8a2ed6b3d966ba711bcc418
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-01-07 03:06:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ function add2(n) {
 
 }
 
-var funkyFunction = function outsideFunction() {
-  return function insideFunction() {
+var funkyFunction = function () {
+  return function {
     return "FUNKY!"
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-console.log(funkyFunction()())
+console.log(funkyFunction())
